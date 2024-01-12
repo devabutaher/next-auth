95260c483fb3fec220f5cc82f64a12ae56def7a4
Abu Taher <writerabutaher@gmail.com>
2024-01-12 23:31:20 +0600

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,76 @@
 import jwt from "jsonwebtoken";
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth, { Account, NextAuthOptions, User } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider, { GithubProfile } from "next-auth/providers/github";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 import { cookies } from "next/headers";
 
 const options: NextAuthOptions = {
   providers: [
+    GithubProvider({
+      profile(profile: GithubProfile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name,
+          email: profile.email,
+          image: profile.avatar_url,
+        };
+      },
+
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+
+    GoogleProvider({
+      profile(profile: GoogleProfile) {
+        const userData = {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+
+        // async () => {
+        //   const res = await fetch("http://localhost:5000/api/users/save", {
+        //     method: "POST",
+        //     body: JSON.stringify(userData),
+        //     headers: { "Content-Type": "application/json" },
+        //   });
+
+        //   const user = await res.json();
+
+        //   if (res.ok && user) {
+        //     const token = jwt.sign(
+        //       { _id: user._id, email: user.email, role: user.role },
+        //       process.env.JWT_SECRET as string,
+        //       {
+        //         expiresIn: "3d",
+        //       }
+        //     );
+
+        //     cookies().set({
+        //       name: "auth-token",
+        //       value: token,
+        //       httpOnly: true,
+        //       path: "/",
+        //       sameSite: "strict",
+        //       maxAge: 3 * 24 * 60 * 60 * 1000,
+        //       secure: process.env.NODE_ENV === "production",
+        //     });
+
+        //     return user;
+        //   } else {
+        //     return null;
+        //   }
+        // };
+
+        return userData;
+      },
+
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+
     CredentialsProvider({
       id: "credentials",
       name: "Credentials",
@@ -57,51 +122,91 @@ const options: NextAuthOptions = {
         return null;
       },
     }),
-
-    GithubProvider({
-      profile(profile: GithubProfile) {
-        // console.log("profile:", profile);
-
-        return {
-          ...profile,
-          id: profile.id.toString(),
-          role: profile.role ?? "user",
-          image: profile.avatar_url,
-        };
-      },
-
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
   ],
-
   callbacks: {
-    // async jwt({ token, user }) {
-    //   if (user) token.role = user.role;
-    //   return token;
-    // },
-    // async session({ session, token }) {
-    //   if (session?.user) session.user.role = token.role;
-    //   return session;
-    // },
-    // async signIn({ user, account }: { user: AuthUser; account: Account }) {
-    //   if (account?.provider == "credentials") {
-    //     try {
-    //       return true;
-    //     } catch (error) {
-    //       console.log("Error saving user", error);
-    //       return false;
-    //     }
-    //   }
-    //   if (account?.provider == "github") {
-    //     try {
-    //       return true;
-    //     } catch (error) {
-    //       console.log("Error saving user", error);
-    //       return false;
-    //     }
-    //   }
-    // },
+    async signIn({ account, profile }) {
+      if (account?.provider == "credentials") {
+        return true;
+      }
+      if (account?.provider == "google") {
+        const userData = {
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          role: "user",
+        };
+
+        const res = await fetch("http://localhost:5000/api/users/save", {
+          method: "POST",
+          body: JSON.stringify(userData),
+          headers: { "Content-Type": "application/json" },
+        });
+
+        const user = await res.json();
+
+        if (res.ok && user) {
+          const token = jwt.sign(
+            { _id: user._id, email: user.email, role: user.role },
+            process.env.JWT_SECRET as string,
+            {
+              expiresIn: "3d",
+            }
+          );
+
+          cookies().set({
+            name: "auth-token",
+            value: token,
+            httpOnly: true,
+            path: "/",
+            sameSite: "strict",
+            maxAge: 3 * 24 * 60 * 60 * 1000,
+            secure: process.env.NODE_ENV === "production",
+          });
+
+          return true;
+        }
+      }
+
+      if (account?.provider == "github") {
+        const userData = {
+          name: profile?.name,
+          email: profile?.email,
+          image: profile?.avatar_url,
+          role: "user",
+        };
+
+        const res = await fetch("http://localhost:5000/api/users/save", {
+          method: "POST",
+          body: JSON.stringify(userData),
+          headers: { "Content-Type": "application/json" },
+        });
+
+        const user = await res.json();
+
+        if (res.ok && user) {
+          const token = jwt.sign(
+            { _id: user._id, email: user.email, role: user.role },
+            process.env.JWT_SECRET as string,
+            {
+              expiresIn: "3d",
+            }
+          );
+
+          cookies().set({
+            name: "auth-token",
+            value: token,
+            httpOnly: true,
+            path: "/",
+            sameSite: "strict",
+            maxAge: 3 * 24 * 60 * 60 * 1000,
+            secure: process.env.NODE_ENV === "production",
+          });
+
+          return true;
+        }
+      }
+      return false;
+    },
   },
 };
 
